ee6cf1c2bf16b54ba6473e341b4ce800d4d9e821
jorgereina1986 <jorgereina1986@gmail.com>
2017-02-15 18:18:18 -0500

    implemented logic for finding characters

--- app/src/main/java/com/jorgereina/hangmanapp/MainActivity.java
+++ app/src/main/java/com/jorgereina/hangmanapp/MainActivity.java
@@ -4,16 +4,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.jorgereina.hangmanapp.models.Player;
-import com.jorgereina.hangmanapp.models.Word;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Random;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -23,40 +20,95 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv;
-
-//    private static final String BASE_URL = "http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/";
+    //    private static final String BASE_URL = "http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/";
     private static final String BASE_URL = "http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words";
 
-    String value = "";
+    private TextView secretWordTv;
+    private TextView updatingTV;
+    private TextView guessCountTV;
+    private EditText guessEt;
+    private Button okBtn;
+
+    String strResponse = "";
 
     private OkHttpClient client;
     private Request request;
     private Handler handler;
-    private String[] words;
-    Word word;
-    Player player;
+    private String[] wordArray;
+    private Random random;
+    String secretWord;
+    String inputLetter;
+    int guessCount = 6;
+    char[] dashedWordArr;
+    String dashedWord;
+
+
+    String[] dummyData = {"hello", "facbook", "minie", "youro", "oown", "businee", "programo", "codingd", "coderc", "androida"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        word = new Word();
-        word.setWord("hello");
-        tv = (TextView) findViewById(R.id.hw_tv);
         handler = new Handler(Looper.getMainLooper());
-        player = new Player();
+        secretWordTv = (TextView) findViewById(R.id.hw_tv);
+        guessEt = (EditText) findViewById(R.id.guess_et);
+        updatingTV = (TextView) findViewById(R.id.updating_tv);
+        okBtn = (Button) findViewById(R.id.ok_btn);
+        guessCountTV = (TextView) findViewById(R.id.counter_tv);
+        random = new Random();
+        wordArray = strResponse.split("\n"); //splitting response in to sting array
+
+//        networkCall();
 
 
+        //getting secret word randomly
+        secretWord = (dummyData[random.nextInt(dummyData.length)]);
+        dashedWord = secretWord;
 
-        networkCall();
+        dashedWord = dashedWord.replaceAll("(?s).", "-");
+        //creating empty word with dashes
+        dashedWordArr = new char[secretWord.length()];
+
+        secretWordTv.setText(secretWord);
+        dashedWordArr = new char[secretWord.length()];
+        for (int i = 0; i < dashedWordArr.length; i++) {
+            dashedWordArr[i] = '-';
+        }
+        updatingTV.setText(dashedWord);
+
+        guessCountTV.setText(guessCount + "");
 
 
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //getting letter that player input
+                inputLetter = guessEt.getText().toString();
+                //turning letter into a char
+                char letter = inputLetter.charAt(0);
+                //check if secret word contains the letter
+                if (secretWord.contains(inputLetter)) {
 
+                    for (int i = 0; i < secretWord.length(); i++) {
 
+                        if (secretWord.charAt(i) == letter) {
 
+                            dashedWordArr[i] = letter;
+
+                            updatingTV.setText(String.valueOf(dashedWordArr));
+
+                        }
+                    }
+                } else {
+                    guessCount--;
+                    guessCountTV.setText(guessCount + "");
+                }
+
+            }
+        });
     }
+
 
     private void networkCall() {
         client = new OkHttpClient();
@@ -73,23 +125,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, final Response response) throws IOException {
 
-                value = response.body().string();
+                strResponse = response.body().string();
 
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
+//                        wordArray = strResponse.split("\n");
+//                        pickWord.setText(Arrays.toString(wordArray));
                     }
                 });
             }
         });
     }
 
-    private void convertResponse(String str){
 
-        List<String> wordList = new ArrayList<>();
-        Collections.addAll(wordList, str.split("\n"));
-        words = str.split("\n");
-        tv.setText(Arrays.toString(words));
+    private void loseGame() {
+        //alert dialog showing that you lost
+        //get new word
+
+    }
+
+    private void winGame() {
+        //get new word
     }
 
 }
+
+
