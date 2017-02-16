dabc02556f7ff46680b2b271b368b5a55c53b933
jorgereina1986 <jorgereina1986@gmail.com>
2017-02-15 21:59:59 -0500

    randomly selecting words from api

--- app/src/main/java/com/jorgereina/hangmanapp/activities/MainActivity.java
+++ app/src/main/java/com/jorgereina/hangmanapp/activities/MainActivity.java
@@ -1,9 +1,8 @@
 package com.jorgereina.hangmanapp.activities;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,19 +11,11 @@ import android.widget.TextView;
 
 import com.jorgereina.hangmanapp.R;
 
-import java.io.IOException;
 import java.util.Random;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     //    private static final String BASE_URL = "http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/";
-    private static final String BASE_URL = "http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words";
 
     private TextView secretWordTv;
     private TextView updatingTV;
@@ -32,41 +23,102 @@ public class MainActivity extends AppCompatActivity {
     private EditText guessEt;
     private Button okBtn;
 
-    String strResponse = "";
-
-    private OkHttpClient client;
-    private Request request;
-    private Handler handler;
-    private String[] wordArray;
     private Random random;
-    String secretWord;
-    String inputLetter;
-    int guessCount = 6;
+    private String secretWord;
+    private String inputLetter;
+    int guessCount = 0;
     char[] dashedWordArr;
-    String dashedWord;
+    private String dashedWord;
 
-
-    String[] dummyData = {"hello", "facbook", "minie", "youro", "oown", "businee", "programo", "codingd", "coderc", "androida"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        handler = new Handler(Looper.getMainLooper());
+
         secretWordTv = (TextView) findViewById(R.id.hw_tv);
         guessEt = (EditText) findViewById(R.id.guess_et);
         updatingTV = (TextView) findViewById(R.id.updating_tv);
         okBtn = (Button) findViewById(R.id.ok_btn);
         guessCountTV = (TextView) findViewById(R.id.counter_tv);
+
+        resetGame();
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //getting letter that player input
+                inputLetter = guessEt.getText().toString();
+                inputLetter = inputLetter.toLowerCase();
+                //turning letter into a char
+                char letter = inputLetter.charAt(0);
+                //check if secret word contains the letter
+                if (secretWord.contains(inputLetter)) {
+                    for (int i = 0; i < secretWord.length(); i++) {
+                        if (secretWord.charAt(i) == letter) {
+                            dashedWordArr[i] = letter;
+                            updatingTV.setText(String.valueOf(dashedWordArr));
+                        }
+                    }
+                    if (secretWord.equals(String.valueOf(dashedWordArr))){
+                        winGame();
+                    }
+                    guessEt.setText("");
+                }
+                else {
+                    guessEt.setText("");
+                    guessCount--;
+                    guessCountTV.setText(guessCount + "");
+                    if (guessCount == 0) {
+
+                        loseGame();
+                    }
+                }
+
+            }
+        });
+    }
+
+    private void loseGame() {
+        //alert dialog showing that you lost
+        //get new word
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("YOU LOSE!")
+                .setMessage("Try Again?")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        resetGame();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    private void winGame() {
+        //get new word
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Hangman")
+                .setTitle("YOU WIN")
+                .setMessage("Try Again?")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        resetGame();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void resetGame(){
+
         random = new Random();
-        wordArray = strResponse.split("\n"); //splitting response in to sting array
-
-//        networkCall();
-
-
+        guessCount = 6;
         //getting secret word randomly
-        secretWord = (dummyData[random.nextInt(dummyData.length)]);
+        secretWord = (SplashScreen.listOfWords[random.nextInt(SplashScreen.listOfWords.length)]);
         dashedWord = secretWord;
 
         dashedWord = dashedWord.replaceAll("(?s).", "-");
@@ -81,86 +133,6 @@ public class MainActivity extends AppCompatActivity {
         updatingTV.setText(dashedWord);
 
         guessCountTV.setText(guessCount + "");
-
-
-        okBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //getting letter that player input
-                inputLetter = guessEt.getText().toString();
-                //turning letter into a char
-                char letter = inputLetter.charAt(0);
-                //check if secret word contains the letter
-                if (secretWord.contains(inputLetter)) {
-
-                    for (int i = 0; i < secretWord.length(); i++) {
-
-                        if (secretWord.charAt(i) == letter) {
-
-                            dashedWordArr[i] = letter;
-
-                            updatingTV.setText(String.valueOf(dashedWordArr));
-
-                        }
-                    }
-                }
-                else {
-                    guessCount--;
-                    guessCountTV.setText(guessCount + "");
-                    if (guessCount == 0) {
-
-
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                        builder.setTitle("Hangman")
-                                .setMessage("You ran out of tries. You Lose. Try Again.")
-                                .setPositiveButton(android.R.string.ok, null);
-                        AlertDialog dialog = builder.create();
-                        dialog.show();
-                    }
-                }
-
-            }
-        });
-    }
-
-
-    private void networkCall() {
-        client = new OkHttpClient();
-        request = new Request.Builder()
-                .url(BASE_URL)
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, final Response response) throws IOException {
-
-                strResponse = response.body().string();
-
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-//                        wordArray = strResponse.split("\n");
-//                        pickWord.setText(Arrays.toString(wordArray));
-                    }
-                });
-            }
-        });
-    }
-
-
-    private void loseGame() {
-        //alert dialog showing that you lost
-        //get new word
-
-    }
-
-    private void winGame() {
-        //get new word
     }
 
 }
---
+++ app/src/main/java/com/jorgereina/hangmanapp/activities/SplashScreen.java
@@ -0,0 +1,124 @@
+package com.jorgereina.hangmanapp.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.jorgereina.hangmanapp.R;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final String BASE_URL = "http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words";
+    private static final int SPLASH_TIME_OUT = 23000;
+
+    private ImageView logoIv;
+    private TextView splashTv;
+
+    private OkHttpClient client;
+    private Request request;
+    private static String wordResponse = "";
+    private Handler handler;
+    private Button button;
+
+    private String[] wordArray;
+
+    static String[] listOfWords;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        logoIv = (ImageView) findViewById(R.id.logo_iv);
+        logoIv.setImageResource(R.mipmap.ic_launcher);
+        splashTv = (TextView) findViewById(R.id.splash_tv);
+        button = (Button) findViewById(R.id.btn);
+
+        handler = new Handler(Looper.getMainLooper());
+
+        networkCall();
+
+        wordArray = wordResponse.split("\n"); //splitting response in to sting array
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        new Handler().postDelayed(new Runnable() {
+//
+//            /*
+//             * Showing splash screen with a timer. This will be useful when you
+//             * want to show case your app logo / company
+//             */
+//
+//            @Override
+//            public void run() {
+//                // This method will be executed once the timer is over
+//                // Start your app main activity
+//                Bundle bundle  = new Bundle();
+//                bundle.putString("word", wordResponse);
+//                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+////                bundle.putStringArray("words",wordArray );
+//                intent.putExtras(bundle);
+//                startActivity(intent);
+//
+//
+//                // close this activity
+//                finish();
+//            }
+//        }, SPLASH_TIME_OUT);
+
+    }
+
+    private void networkCall() {
+        client = new OkHttpClient();
+        request = new Request.Builder()
+                .url(BASE_URL)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+
+                wordResponse = response.body().string();
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        listOfWords = wordResponse.split("\n");
+                        splashTv.setText(Arrays.toString(listOfWords));
+                    }
+                });
+            }
+        });
+
+    }
+
+
+}
